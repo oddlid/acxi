fdc9479ae66408a1fe9a60354d65d935b2b4422b
Odd Eivind Ebbesen <odd@oddware.net>
2011-04-21 04:11:30 +0200

    Added dircopy and dircopy_helper, which creates destination directories if needed. Also converted a lot of double quoted strings to qq() as Perl guidelines says one should use.

--- acxi_v3_odd.pl
+++ acxi_v3_odd.pl
@@ -62,6 +62,8 @@ Print acxi version number, and some more details.
 use strict;
 use warnings;
 use Getopt::Long qw(:config auto_version auto_help no_ignore_case);
+use File::Find;
+use Cwd;
 
 
 ## Internal settings:
@@ -72,36 +74,36 @@ $main::VERSION = "3.0.0";
 # Not used yet, but as a future thought...
 my %ACXI = (
     version     => $main::VERSION, 
-    date        => '2011-04-15',
-    desc        => 'Audio file conversion script', 
-    authors     => 'Harald Hope, Odd Eivind Ebbesen', 
-    credits     => 'Jason L. Buberel <jason@buberel.org>, Evan Boggs <etboggs@indiana.edu>', 
-    url         => 'http://techpatterns.com/forums/about1491.html'
+    date        => q(2011-04-15),
+    desc        => q(Audio file conversion script), 
+    authors     => q(Harald Hope, Odd Eivind Ebbesen), 
+    credits     => q(Jason L. Buberel <jason@buberel.org>, Evan Boggs <etboggs@indiana.edu>), 
+    url         => q(http://techpatterns.com/forums/about1491.html)
 );
 # Internal log/message levels (use corresponding values, not variables in config files)
 my %LOG = (
     quiet       => 0, 
     info        => 1, 
-    warn        => 2, 
+    verbose     => 2, 
     debug       => 3
 );
 
 my %LINE = (
-    small => '-----------------------------------------------------------------\n', 
-    large => '---------------------------------------------------------------------------\n', 
-    heavy => '===========================================================================\n'
+    small => qq(-----------------------------------------------------------------\n), 
+    large => qq(---------------------------------------------------------------------------\n), 
+    heavy => qq(===========================================================================\n)
 );
 
 my @CONFIGS = (
-    "/etc/acxi.conf", 
-    "$ENV{HOME}/.acxi.conf"
+    qq(/etc/acxi.conf), 
+    qq($ENV{HOME}/.acxi.conf)
 );
 
 my @OUTPUT_TYPES = ("ogg", "mp3");
 
-my %ARG_ENCODER_VERBOSITY = (
-    mp3 => %{$LOG{QUIET} => "--quiet";
-);
+#my %ARG_ENCODER_VERBOSITY = (
+#    mp3 => %{$LOG{QUIET} => "--quiet";
+#);
 ## User settings:
 # In the config file, you just write KEY = value, eg:
 # DIR_PREFIX_SOURCE = /home/user/flac 
@@ -127,6 +129,8 @@ my %USER_SETTINGS = (
 ## Functions:
 
 sub acxi_log {
+    # The result of calling this function without a level param, is that
+    # the message is shown at which ever level is set, except "quiet",
     my ($msg, $lvl) = @_;
     if (!defined($lvl)) {
         $lvl = $USER_SETTINGS{LOG_LEVEL};
@@ -141,6 +145,8 @@ sub acxi_log {
 
 sub read_config_file {
     my ($file, $var, $val);
+    # Config files should be passed in an array as a param to this function.
+    # Default intended use: global @CONFIGS;
     foreach $file (@_) {
         next unless open (CONFIG, "$file");
         while (<CONFIG>) {
@@ -155,12 +161,34 @@ sub read_config_file {
     }
 }
 
-sub set_arg_audio {
-    my ($llvl, $itype, $otype) = @_;
-    if ($llvl eq $LOG{quiet}) {
-        $ARG_ENCODER{$otype} = "--silent";
+# Create destination directories as in source
+sub dircopy {
+    acxi_log($LINE{large}, $LOG{verbose});
+    acxi_log(qq(Syncing source and destinations directories...\n), $LOG{verbose});
+    find(\&dircopy_helper, $USER_SETTINGS{DIR_PREFIX_SOURCE});
+    acxi_log(qq(Directory syncronization complete.\n), $LOG{verbose});
+}
+
+# Helper function for File::Find ("wanted")
+sub dircopy_helper {
+    # not symlink, is dir, not . or .. and no newline
+    if (! -l && -d && ! /^\.{1,2}$/ && ! /\n$/) {
+        my $srcx = qq($USER_SETTINGS{DIR_PREFIX_SOURCE});
+        my $dstx = qq($USER_SETTINGS{DIR_PREFIX_DEST});
+        my $newdir = $File::Find::name;
+        $newdir =~ s/$srcx/$dstx/;
+        acxi_log($LINE{small}, $LOG{verbose});
+        acxi_log(qq(Creating new directory:\n\t$newdir\n), $LOG{verbose});
+        mkdir(qq($newdir)) or warn(qq(Failed to create directory: "$newdir" -  $!));
     }
 }
+
+#sub set_arg_audio {
+#    my ($llvl, $itype, $otype) = @_;
+#    if ($llvl eq $LOG{quiet}) {
+#        $ARG_ENCODER{$otype} = "--silent";
+#    }
+#}
 
 ## Entry point:
 
@@ -173,9 +201,10 @@ GetOptions(
 
 read_config_file(@CONFIGS);
 while (my ($k, $v) = each %USER_SETTINGS) {
-    print("$k => $v\n");
+    acxi_log(qq($k => $v\n), $LOG{debug});
 }
 
-acxi_log("Ladidadida, logging at user or predefined level ($USER_SETTINGS{LOG_LEVEL})\n");
-acxi_log("Logging at DEBUG, which should not be seen if level < 3\n", $LOG{debug});
+acxi_log(qq(Ladidadida, logging at user or predefined level ($USER_SETTINGS{LOG_LEVEL})\n));
+acxi_log(qq(Logging at DEBUG, which should not be seen if level < 3\n), $LOG{debug});
 
+dircopy();
